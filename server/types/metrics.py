eecece1a037ec534eabff47a9ca1def63dd7373e
Rahul Butani <rahul@centtech.com>
2019-06-14 17:11:58 -0500

    server: touch up Metrics

--- server/types/metrics.py
+++ server/types/metrics.py
@@ -1,5 +1,24 @@
-from server.types import Metrics
+from server.types import Metrics as MetricsMessage
+from __future__ import annotations
 
-# TODO: add overloaded functions once we have traces and such
-def into_metrics(time_to_execute: int) -> Metrics:
-    return Metrics(time_to_execute=time_to_execute)
+class Metrics:
+    def __init__(self, time_to_execute: int = 0, trace_url: str = ""):
+        self.time_to_execute(time_to_execute)
+        self.trace_url(trace_url)
+
+    def time_to_execute(self, time_to_execute: int) -> LocalMetrics:
+        assert time_to_execute >= 0
+
+        self._time_to_execute = time_to_execute
+        return self
+
+    def trace(self, trace: str) -> LocalMetrics:
+        self._trace = trace
+        return self
+
+    def into_metrics(self) -> Metrics:
+        return Metrics(time_to_execute=self._time_to_execute, trace=self._trace)
+
+# # TODO: add overloaded functions once we have traces and such
+# def into_metrics(metrics: LocalMetrics) -> Metrics:
+#     return Metrics(time_to_execute=metrics.)

e65ae679799e7a8fdce133dc09553cf3ea849cd4
Rahul Butani <rahul@centtech.com>
2019-06-18 16:26:22 -0500

    client: first pass at protobufjs

---
+++ examples/client/scripts/build-messages.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+/**
+ * Quick and simple script that builds the protobuf messages.
+ */
+
+import { dirname } from 'path';
+import { mkdirSync as mkdir } from 'fs';
+
+import { pbjs, pbts } from 'protobufjs/cli';
+
+import pkg from './../package.json';
+
+const config = pkg.config;
+
+const message_dir = config.message_dir || "messages";
+const message_file = config.message_file || "build/messages.js";
+const message_types = config.message_types || "build/types/messages.d.ts";
+
+const throw_err = (err: Error, _: string) => { if (err) throw err; }
+const create = (file: string) => mkdir(dirname(file), { "recursive": true });
+
+create(message_file)
+create(message_types)
+
+pbjs.main(
+  [ "--target"
+  , "static-module"
+  , "--wrap"
+  , "es6"
+  , "--es6"
+  , "--keep-case"
+  , "--path"
+  , message_dir
+  , "--out"
+  , message_file
+  , `${message_dir}/*.proto`
+  ]
+, throw_err
+);
+
+pbts.main(
+  [ message_file
+  , "--out"
+  , message_types
+  ]
+, throw_err
+)

5acad854b9747cc855f41e7f835d7273142d3f51
Rahul Butani <rahul@centtech.com>
2019-08-08 10:45:00 -0500

    server: model loading/conversion, part 2

--- server/types/model.py
+++ server/types/model.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, Callable, Optional, NoReturn as Never
 from os import environ
-from os.path import join
+from os.path import dirname, join
 from tempfile import mkdtemp
 import urllib
 from urllib.request import urlretrieve as download
@@ -9,6 +9,14 @@ from shutil import rmtree, copyfile
 
 from ..types import Model, ModelHandle
 from ..types.Model.Type import *
+
+from tensorflowjs.converters.converter import (
+    dispatch_keras_saved_model_to_tensorflowjs_conversion,
+    dispatch_tensorflowjs_to_keras_h5_conversion,
+    dispatch_keras_h5_to_tensorflowjs_conversion,
+)
+
+from tensorflow.compat.v1.lite import TFLiteConverter
 
 LocalHandle = int
 
@@ -36,11 +44,11 @@ model_type_to_path: Dict[ModelType, str] = {
     TF_SAVED_MODEL:     "tf_saved_model/",
     KERAS_HDF5:         "keras_model.h5",
     KERAS_SAVED_MODEL:  "keras_saved_model/",
-    KERAS_OTHER:        "",
+    KERAS_OTHER:        "keras_model_other.h5",
     TFJS_LAYERS:        "tfjs_layers_model.json",
     TFJS_GRAPH:         "tfjs_graph_model/",
-    TF_HUB:             "",
-    GRAPH_DEFS:         "",
+    TF_HUB:             "tf_hub_model.tfhub", # placeholder
+    GRAPH_DEFS:         "graph_defs.gdefs", # placeholder
 }
 # fmt: on
 
@@ -61,7 +69,8 @@ model_conversion_steps: Dict[ModelType, ConversionFunc] = {
 
 def get_path_for_model_type(model_type: ModelType, directory: str) -> str:
     """
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
+    :raises ModelConversionError: When path information for the given model
+                                  type isn't available.
     """
     model = model_type_to_path.get(model_type)
 
@@ -76,13 +85,16 @@ n: Callable[[ModelType], str] = name_model_type = lambda type: Model.Type.Name(t
 
 def _unimplemented(model_type: ModelType) -> Never:
     """
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
+    :raises ModelConversionError: Always. Signifies that conversion for the
+                                  specified model has yet to be implemented.
     """
     raise ModelConversionError(f"Sorry! Converting `{n(model_type)}` models isn't supported yet.")
 
 def conversion_step(model_type: ModelType, directory: str) -> str:
     """
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
+    :raises ModelConversionError: When given a model that we don't know how to
+                                  convert or when errors occur during model
+                                  conversion.
     """
     func: Optional[ConversionFunc] = model_conversion_steps.get(model_type)
     model: str = get_path_for_model_type(model_type, directory)
@@ -90,74 +102,70 @@ def conversion_step(model_type: ModelType, directory: str) -> str:
     if func is None:
         raise ModelConversionError(f"Unsupported model type (`{n(model_type)}`): No conversion function available!")
 
-    return func(directory, model)
+    try:
+        return func(directory, model)
+    except ModelConversionError as e:
+        raise e # Pass this along unaltered
+    except Exception as e:
+        raise ModelConversionError(f"Hit an error converting a `{n(model_type)}` model: {e}")
 
 def tf_saved_model_to_tflite(directory: str, input_dir: str) -> str:
-    """
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
-    """
     target = TFLITE_FLAT_BUFFER
     output = p(target, directory)
 
-    ...
-    TODO
+    tflite = TFLiteConverter.from_saved_model(input_dir).convert()
+
+    with open(output, "wb") as f:
+        f.write(tflite)
 
     return conversion_step(target, directory)
 
-def keras_hdf5_to_tflite(directory: str, input_dir: str) -> str:
-    """
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
-    """
+def keras_hdf5_to_tflite(directory: str, input_file: str) -> str:
     target = TFLITE_FLAT_BUFFER
     output = p(target, directory)
 
-    ...
-    TODO
+    tflite = TFLiteConverter.from_keras_model_file(input_file).convert()
+
+    with open(output, "wb") as f:
+        f.write(tflite)
 
     return conversion_step(target, directory)
 
 def keras_saved_model_to_tfjs_layers(directory: str, input_dir: str) -> str:
-    """
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
-    """
     target = TFJS_LAYERS
     output = p(target, directory)
+    output_dir = join(dirname(output), "tfjs-layers-model")
 
-    ...
-    TODO
+    dispatch_keras_saved_model_to_tensorflowjs_conversion(input_dir, output_dir)
+    copyfile(join(output_dir, "saved_model.json"), output)
 
     return conversion_step(target, directory)
 
-def keras_other_to_tfjs_layers(directory: str, input_dir: str) -> str:
-    """
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
-    """
+def keras_other_to_tfjs_layers(directory: str, input_file: str) -> str:
     target = TFJS_LAYERS
     output = p(target, directory)
+    output_dir = join(dirname(output), "tfjs-layers-model")
 
-    ...
-    TODO
+    dispatch_keras_h5_to_tensorflowjs_conversion(input_file, output_dir=output_dir)
+    copyfile(join(output_dir, "saved_model.json"), output)
 
     return conversion_step(target, directory)
 
-def tfjs_layers_to_keras_hdf5(directory: str, input_dir: str) -> str:
-    """
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
-    """
+def tfjs_layers_to_keras_hdf5(directory: str, input_file: str) -> str:
     target = KERAS_HDF5
     output = p(target, directory)
 
-    ...
-    TODO
+    dispatch_tensorflowjs_to_keras_h5_conversion(input_file, output)
 
     return conversion_step(target, directory)
 
-# TODO!!!
 def convert_model(model: Model) -> str:
     """
-    :raises ModelAcquireError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
-    :raises ModelConversionError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
-    :raises ModelDataError <TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
+    :raises ModelAcquireError: When a model cannot be fetched.
+    :raises ModelConversionError: When given a model that we don't know how to
+                                  convert or when errors occur during model
+                                  conversion.
+    :raises ModelDataError: On invalid model messages.
     """
 
     # Check that we've got a data source:
